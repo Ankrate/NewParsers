8d4fd903c1aa5e8ea2ac3b2c8697aac346ab2166
Ankrate <yellkalolka@gmail.com>
2018-11-29 14:46:34 +0300

    готовый кусок для парсинга хар-к товара + db

--- Documents/parsers/speckom/speckom_goods.py
+++ Documents/parsers/speckom/speckom_goods.py
@@ -12,8 +12,26 @@ socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
 socket.socket = socks.socksocket
 
 
-def parse_goods():
-    html = requests.get('http://speckomzapchast.ru/hyundai-porter/dvigatel/balansirovochnyj-val/vtulka-vala-balansirovochnogo.html').content
+def takeid():
+    connection = sqlite3.connect("/home/user500/Desktop/specmash.db")
+    cur = connection.cursor()
+    todbZapros = ("SELECT url FROM urls")
+    cur.execute(todbZapros)
+    rows = cur.fetchall()
+    connection.commit()
+    return(rows)
+
+
+def todb(url,name,number,analog_number,brand,price,primen,car,seller):
+    connection = sqlite3.connect("/home/user500/Desktop/specmash.db")
+    cur = connection.cursor()
+    todbValue = (url,name,number,analog_number,brand,price,primen,car,seller)
+    query = ("INSERT INTO urls(url,name,number,analog_number,brand,price,primen,car,seller) VALUES (?,?,?,?,?,?,?,?,?)")
+    cur.execute(query,todbValue)
+    connection.commit()
+
+def parse_goods(url):
+    html = requests.get('http://speckomzapchast.ru' + url).content
     soup = BeautifulSoup(html, "lxml")
     name = soup.find('h1', {'itemprop':'name'}).text
     print(name)
@@ -26,12 +44,12 @@ def parse_goods():
     analog_number = re.sub("Аналоги:", "", list1[2].text)
     car = re.sub("Автомобиль:", "", list1[3].text)
     primen = re.sub("Применяемость:", "", list1[4].text)
+    seller = "7"
     print(number)
     print(brand)
     print(analog_number)
     print(car)
     print(primen)
-    # for lis in list1:
-    #     print(lis.text)
-    #
+    todb(url,name,number,analog_number,brand,price,primen,car,seller)
+
 parse_goods()
