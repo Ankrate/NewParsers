9432f86a3db7ad7873c513c62d6480df6eb2b5d6
Ankrate <yellkalolka@gmail.com>
2018-11-29 14:14:12 +0300

    готовый кусок для парсинга хар-к товара

--- Documents/parsers/speckom/speckom_goods.py
+++ Documents/parsers/speckom/speckom_goods.py
@@ -12,12 +12,26 @@ socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
 socket.socket = socks.socksocket
 
 
-def parse_start():
+def parse_goods():
     html = requests.get('http://speckomzapchast.ru/hyundai-porter/dvigatel/balansirovochnyj-val/vtulka-vala-balansirovochnogo.html').content
     soup = BeautifulSoup(html, "lxml")
     name = soup.find('h1', {'itemprop':'name'}).text
     print(name)
     price = re.sub(r"[^\d]", "", soup.find('div', {'class':'price'}).text)
     print(price)
-
-parse_start()
+    harak = soup.find('div', {'class': 'harak'}).table
+    list1 = harak.findAll('tr')
+    number = re.sub("Артикул:", "", list1[0].text)
+    brand = re.sub("Производитель:", "", list1[1].text)
+    analog_number = re.sub("Аналоги:", "", list1[2].text)
+    car = re.sub("Автомобиль:", "", list1[3].text)
+    primen = re.sub("Применяемость:", "", list1[4].text)
+    print(number)
+    print(brand)
+    print(analog_number)
+    print(car)
+    print(primen)
+    # for lis in list1:
+    #     print(lis.text)
+    #
+parse_goods()
