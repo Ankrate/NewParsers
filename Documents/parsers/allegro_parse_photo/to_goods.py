aa4c20d2d8bc90c517fdb3d43ec8119fadad2277
Ankrate <yellkalolka@gmail.com>
2018-12-25 12:23:03 +0300

    Добавил парсер ссылок с поисковой выдачи Аллегро Добавил парсер страниц с товаром по ссылкам

--- Documents/parsers/allegro_parse_photo/to_goods.py
+++ Documents/parsers/allegro_parse_photo/to_goods.py
@@ -12,40 +12,53 @@ import re
 # socket.socket = socks.socksocket
 
 
-# def takeid():
-#     connection = sqlite3.connect("/home/user500/speckom.db")
-#     cur = connection.cursor()
-#     todbZapros = ("SELECT goods_url FROM urls")
-#     cur.execute(todbZapros)
-#     rows = cur.fetchall()
-#     connection.commit()
-#     return(rows)
+def takeid():
+    connection = sqlite3.connect("/home/user500/allegro_photo.db")
+    cur = connection.cursor()
+    todbZapros = ("SELECT article FROM articles_of_parts LIMIT 1")
+    cur.execute(todbZapros)
+    rows = cur.fetchall()
+    connection.commit()
+    return(rows)
 
 
-# def todb(url,name,number,analog_number,brand,price,primen,car,seller):
-#     connection = sqlite3.connect("/home/user500/speckom.db")
-#     cur = connection.cursor()
-#     todbValue = (url,name,number,analog_number,brand,price,primen,car,seller)
-#     query = ("INSERT INTO main(url,name,number,analog_number,brand,price,description,car,seller) VALUES (?,?,?,?,?,?,?,?,?)")
-#     cur.execute(query,todbValue)
-#     connection.commit()
+def todb(article,img,description):
+    connection = sqlite3.connect("/home/user500/allegro_50.db")
+    cur = connection.cursor()
+    todbValue = (article,img,description)
+    query = ("INSERT INTO main(article,img,description) VALUES (?,?,?)")
+    cur.execute(query,todbValue)
+    connection.commit()
 
-def parse_goods(url):
+def parse_links(article):
     #html = requests.get(url).content
-    html = urlopen(url)
+    html = urlopen("https://allegro.pl/listing?string=" + article)
     soup = BeautifulSoup(html, "lxml")
+
     table = soup.findAll('article', {'data-item':'true'})
     for strokazap in table:
         urls = strokazap.findAll('a')
-        print(urls[0]['href'])
+        pageUrl = urls[0]['href']
 
-    time.sleep(2)
+        parse_page(article,pageUrl)
 
 
 
-#parse_goods("https://allegro.pl/listing?string=" + "504043884")
-parse_goods("https://allegro.pl/listing?string=" + "504183759")
-# rows = takeid()
-# for links in rows:
-#     parse_goods(links[0])
-#     time.sleep(1)
+def parse_page(article,pageUrl):
+    html = urlopen(pageUrl)
+    soup = BeautifulSoup(html, "lxml")
+
+    photoLink = soup.find('meta',{'itemprop':'image'})
+    description = str(soup.find('div', 'description'))
+    img = photoLink['content']
+    todb(article,img,description)
+
+
+#parse_page('https://allegro.pl/oferta/wahacz-delphi-citroen-jumper-7195753988')
+#parse_goods("https://allegro.pl/listing?string=" + "504183759")
+
+
+articles = takeid()
+for article in articles:
+    parse_links(article[0])
+
